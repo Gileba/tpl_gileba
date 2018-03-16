42f24614630dde17c1ad790d368bed63587f1035
Gijs Lamon <Gileba@users.noreply.github.com>
2018-03-16 10:34:12 +0100

    The most simple template

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @package     Joomla.Site
+ * @subpackage  Templates.gileba
+ *
+ * @copyright   Copyright (C) 2018 Gileba. All rights reserved.
+ * @license     GNU General Public License version 3 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+?>
+<!DOCTYPE html>
+<html>
+<body>
+  <div>
+    <jdoc:include type="component" />
+  </div>
+</body>
+</html>
