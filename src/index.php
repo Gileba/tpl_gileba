aceacb8c9add26ddc9e1f597b91f221895ff4a8c
Gijs Lamon <info@gileba.be>
2021-02-11 09:48:17 +0100

    Codefactor

--- src/index.php
+++ src/index.php
@@ -20,25 +20,25 @@ $sitename = htmlspecialchars($app->get('sitename'), ENT_QUOTES, 'UTF-8');
 *	Mobile device detection
 */
 if( !function_exists('mobile_user_agent_switch') ){
-function mobile_user_agent_switch(){
-	$device = '';
+	function mobile_user_agent_switch(){
+		$device = '';
 	
-	if( stristr($_SERVER['HTTP_USER_AGENT'],'ipad') ) {
-		$device = "ipad";
-	} else if( stristr($_SERVER['HTTP_USER_AGENT'],'iphone') || strstr($_SERVER['HTTP_USER_AGENT'],'iphone') ) {
-		$device = "iphone";
-	} else if( stristr($_SERVER['HTTP_USER_AGENT'],'blackberry') ) {
-		$device = "blackberry";
-	} else if( stristr($_SERVER['HTTP_USER_AGENT'],'android') ) {
-		$device = "android";
-	}
+		if( stristr($_SERVER['HTTP_USER_AGENT'],'ipad') ) {
+			$device = "ipad";
+		} else if( stristr($_SERVER['HTTP_USER_AGENT'],'iphone') || strstr($_SERVER['HTTP_USER_AGENT'],'iphone') ) {
+			$device = "iphone";
+		} else if( stristr($_SERVER['HTTP_USER_AGENT'],'blackberry') ) {
+			$device = "blackberry";
+		} else if( stristr($_SERVER['HTTP_USER_AGENT'],'android') ) {
+			$device = "android";
+		}
 	
-	if( $device ) {
-		return "mobile"; 
-	} return false; {
-		return false;
+		if( $device ) {
+			return "mobile"; 
+		} return false; {
+			return false;
+		}
 	}
-}
 }
 ?>
 
