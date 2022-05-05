802a12e1b62cbff6e50f8d59c95e6eabe3a1f5ad
Gijs Lamon <info@gileba.be>
2022-05-05 17:38:22 +0200

    Code cleanup

--- src/index.php
+++ src/index.php
@@ -1,23 +1,16 @@
 <?php
 defined('_JEXEC') or die('Restricted access');
 
+use Joomla\CMS\Factory;
+
 /** @var JDocumentHtml $this */
-$app      	= JFactory::getApplication();
-$doc      	= JFactory::getDocument();
-
-/** Output as HTML5 */
-$this->setHtml5(true);
-
+$app      	= Factory::getApplication();
 $menu 		= $app->getMenu();
 $params		= $app->getTemplate(true)->params;
-$config 	= JFactory::getConfig();
 $pageclass = "";
 if ($menu->getActive() != null) {
 	$pageclass 	= $menu->getActive()->getParams(true)->get('pageclass_sfx');
 }
-
-// Logo file or site title param
-$sitename = htmlspecialchars($app->get('sitename'), ENT_QUOTES, 'UTF-8');
 
 /*
 *	Mobile device detection
@@ -104,25 +97,25 @@ jQuery( document ).ready(function() {
 				</a>
 			</div>
 			<div class="navigation">
-				<jdoc:include type="modules" name="nav" />
+				<jdoc:include type="modules" name="nav" style="none" />
 			</div>
 		</div>
 		<div class="home">
-			<?php if ($menu->getActive() == $menu->getDefault()) : ?><jdoc:include type="modules" name="home" /><?php
+			<?php if ($menu->getActive() == $menu->getDefault()) : ?><jdoc:include type="modules" name="home" style="none" /><?php
 			endif ?>
 		</div>
 		<div class="component">
-			<?php if ($menu->getActive() != $menu->getDefault()) : ?><jdoc:include type="component" /><?php
+			<?php if ($menu->getActive() != $menu->getDefault()) : ?><jdoc:include type="component" style="none" /><?php
 			endif ?>
 		</div>
 	</div>
 	<?php if ($menu->getActive() == $menu->getDefault()) : ?>
 	<div id="contact" class="contact">
-		<jdoc:include type="modules" name="contact" />
+		<jdoc:include type="modules" name="contact" style="none" />
 	</div>
 	<?php endif ?>
 	<div class="footer">
-		<jdoc:include type="modules" name="footer" />
+		<jdoc:include type="modules" name="footer" style="none" />
 	</div>
 </body>
 </html>
