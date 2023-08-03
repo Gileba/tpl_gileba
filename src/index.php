9693b3d58cba5dcfd7a5c672ac6c44935182d123
Gijs Lamon <info@gileba.be>
2023-08-03 13:24:30 +0200

    Add “intro” position

--- src/index.php
+++ src/index.php
@@ -1,41 +1,44 @@
 <?php
-defined('_JEXEC') or die('Restricted access');
+defined("_JEXEC") or die("Restricted access");
 
 use Joomla\CMS\Factory;
 
 /** @var JDocumentHtml $this */
-$app      	= Factory::getApplication();
-$menu 		= $app->getMenu();
-$params		= $app->getTemplate(true)->params;
-$lang 		= $app->getLanguage();
+$app = Factory::getApplication();
+$menu = $app->getMenu();
+$params = $app->getTemplate(true)->params;
+$lang = $app->getLanguage();
 $pageclass = "";
 if ($menu->getActive() != null) {
-	$pageclass 	= $menu->getActive()->getParams(true)->get('pageclass_sfx');
+	$pageclass = $menu
+		->getActive()
+		->getParams(true)
+		->get("pageclass_sfx");
 }
 
 /*
-*	Mobile device detection
-*/
-if(!function_exists('mobile_user_agent_switch')){
+ *	Mobile device detection
+ */
+if (!function_exists("mobile_user_agent_switch")) {
 	function mobile_user_agent_switch()
 	{
-		$device = '';
+		$device = "";
 
-		if(stristr($_SERVER['HTTP_USER_AGENT'], 'ipad')) {
+		if (stristr($_SERVER["HTTP_USER_AGENT"], "ipad")) {
 			$device = "ipad";
-		} elseif(stristr($_SERVER['HTTP_USER_AGENT'], 'iphone') || strstr($_SERVER['HTTP_USER_AGENT'], 'iphone')) {
+		} elseif (stristr($_SERVER["HTTP_USER_AGENT"], "iphone") || strstr($_SERVER["HTTP_USER_AGENT"], "iphone")) {
 			$device = "iphone";
-		} elseif(stristr($_SERVER['HTTP_USER_AGENT'], 'blackberry')) {
+		} elseif (stristr($_SERVER["HTTP_USER_AGENT"], "blackberry")) {
 			$device = "blackberry";
-		} elseif(stristr($_SERVER['HTTP_USER_AGENT'], 'android')) {
+		} elseif (stristr($_SERVER["HTTP_USER_AGENT"], "android")) {
 			$device = "android";
 		}
 
-		if($device) {
+		if ($device) {
 			return "mobile";
-		} return false; {
-			return false;
 		}
+		return false;
+		return false;
 	}
 }
 ?>
@@ -47,8 +50,8 @@ xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>" 
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="shortcut icon" type="image/png" href="/templates/<?php echo $this->template; ?>/images/favicon.png" />
 	<jdoc:include type="head" />
-	<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/css/template.css" type="text/css" />
-<?php	if ($this->params->get('tagmanager')) {	?>
+	<link rel="stylesheet" href="<?php echo $this->baseurl; ?>/templates/<?php echo $this->template; ?>/css/template.css" type="text/css" />
+<?php if ($this->params->get("tagmanager")) { ?>
 <!-- Google Tag Manager -->
 	<script>
 		(function(w,d,s,l,i){
@@ -59,7 +62,7 @@ xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>" 
 			j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
 			f.parentNode.insertBefore(j,f);
 		})
-		(window,document,'script','dataLayer','<?php	echo $this->params->get('tagmanager'); ?>');
+		(window,document,'script','dataLayer','<?php echo $this->params->get("tagmanager"); ?>');
 	</script>
 	<!-- End Google Tag Manager -->
 	<script>
@@ -79,37 +82,43 @@ jQuery( document ).ready(function() {
 	}
 });
 	</script>
-<?php }; ?>
+<?php } ?>
 </head>
-<body class="<?php echo $pageclass ? htmlspecialchars($pageclass) : 'default'; ?> <?php echo mobile_user_agent_switch(); ?>">
-<?php	if ($this->params->get('tagmanager')) {	?>
+<body class="<?php echo $pageclass
+	? htmlspecialchars($pageclass)
+	: "default"; ?> <?php echo mobile_user_agent_switch(); ?>">
+<?php if ($this->params->get("tagmanager")) { ?>
 	<!-- Google Tag Manager (noscript) -->
 	<noscript>
-		<iframe src="https://www.googletagmanager.com/ns.html?id=<?php	echo $this->params->get('tagmanager'); ?>"
+		<iframe src="https://www.googletagmanager.com/ns.html?id=<?php echo $this->params->get("tagmanager"); ?>"
 			height="0" width="0" style="display: none; visibility: hidden;">
 		</iframe>
 	</noscript>
 	<!-- End Google Tag Manager (noscript) -->
-<?php }; ?>
+<?php } ?>
 	<div class="container">
 		<div class="top">
 			<div class="logo">
 				<a href="/">
-					<img src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/images/logo_liggend.png" alt="Gileba" />
+					<img src="<?php echo $this->baseurl; ?>/templates/<?php echo $this->template; ?>/images/logo_liggend.png" alt="Gileba" />
 				</a>
 			</div>
 			<div class="navigation">
 				<jdoc:include type="modules" name="nav" style="none" />
 			</div>
 		</div>
-		<?php if ($menu->getActive() == $menu->getDefault($lang->getTag())) : ?>
+		<div class="intro">
+			<jdoc:include type="modules" name="intro" style="none" />
+		</div>
+		<?php if ($menu->getActive() == $menu->getDefault($lang->getTag())): ?>
 		<div class="home">
 			<jdoc:include type="modules" name="home" style="none" />
 		</div>
-		<?php endif ?>
+		<?php endif; ?>
 		<div class="component">
-			<?php if ($menu->getActive() != $menu->getDefault($lang->getTag())) : ?><jdoc:include type="component" style="none" /><?php
-			endif ?>
+			<?php if (
+   	$menu->getActive() != $menu->getDefault($lang->getTag())
+   ): ?><jdoc:include type="component" style="none" /><?php endif; ?>
 		</div>
 	</div>
 	<div id="contact" class="contact">
